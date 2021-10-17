9a031e504358d4e964aa22c5bf37b71649b21f84
Richarzone <Richarzonea28@gmail.com>
2021-10-17 15:51:09 -0500

    prueba conexion con api

--- src/Views/CalendarioView.js
+++ src/Views/CalendarioView.js
@@ -4,6 +4,15 @@ import Nav from '../Navigation';
 import { Lista } from "../Components/Lista";
 
 export class Calendario extends Component {
+
+    async componentDidMount()
+    {
+        const url = "localhost:4000/agendarVisita/getDias";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    }
+
     render() {
         return (
             <div className="App">
@@ -11,7 +20,6 @@ export class Calendario extends Component {
                 <div className="">
                     <Lista/>
                 </div>
-                
             </div>
         );
     }
