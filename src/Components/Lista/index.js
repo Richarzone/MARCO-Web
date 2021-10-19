09b4df754533580077cdbef97038889f6c949a7f
Ricardo Arriaga <richarzonea28@gmail.com>
2021-10-19 13:01:44 -0500

    calendario lunes cerrado

--- src/Components/Lista/index.js
+++ src/Components/Lista/index.js
@@ -5,34 +5,54 @@ import "./Lista.css"
 
 export class Lista extends Component {
     render() {
-        return(
-            <Accordion allowZeroExpanded className="accordion">
-                <AccordionItem>
-                    <AccordionItemHeading>
-                        <AccordionItemButton className="accordion__button">
-                            {this.props.Dia}
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel className="accordion__panel">
-                    {
-                        this.props.Bloques[0].map((bloque) => (
-                            <Accordion allowZeroExpanded className="sub_accordion">
-                                <AccordionItem>
-                                    <AccordionItemHeading>
-                                        <AccordionItemButton className="sub_accordion__button">
-                                            Bloque {bloque.numero}
-                                        </AccordionItemButton>
-                                    </AccordionItemHeading>
-                                    <AccordionItemPanel className="accordion__panel">
-                                            Hora de Inicio: {bloque.horaInicio}
-                                    </AccordionItemPanel>
-                                </AccordionItem>
-                            </Accordion>
-                        ))
-                    }
-                    </AccordionItemPanel>
-                </AccordionItem>
-            </Accordion>
-        )
+        if(this.props.Dia != "Lunes")
+        {
+            return(
+                <Accordion allowZeroExpanded className="accordion">
+                    <AccordionItem>
+                        <AccordionItemHeading>
+                            <AccordionItemButton className="accordion__button">
+                                {this.props.Dia}
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel className="accordion__panel">
+                        {
+                            this.props.Bloques[0].map((bloque) => (
+                                <Accordion allowZeroExpanded className="sub_accordion">
+                                    <AccordionItem>
+                                        <AccordionItemHeading>
+                                            <AccordionItemButton className="sub_accordion__button">
+                                                Bloque {bloque.numero}
+                                            </AccordionItemButton>
+                                        </AccordionItemHeading>
+                                        <AccordionItemPanel className="accordion__panel">
+                                                Hora de Inicio: {bloque.horaInicio}
+                                        </AccordionItemPanel>
+                                    </AccordionItem>
+                                </Accordion>
+                            ))
+                        }
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                </Accordion>
+            )
+        }
+        else
+        {
+            return(
+                <Accordion allowZeroExpanded className="accordion">
+                    <AccordionItem>
+                        <AccordionItemHeading>
+                            <AccordionItemButton className="accordion__button">
+                                {this.props.Dia}
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel className="accordion__panel">
+                            Cerrado
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                </Accordion>
+            )
+        }
     }
 }
