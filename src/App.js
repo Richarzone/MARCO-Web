3645d35e75c97b82fca2a2476908dea6da3ad723
joelreyna1104 <joelreyna1104@gmail.com>
2021-10-20 22:31:01 -0500

    vistas de horarios y guias

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Views/HomeView';
 import AdminVisitas from './Views/AdminVisitasView';
 import Exposiciones from './Views/Exposiciones';
 import Guias from './Views/GuiasView';
+import Horarios from './Views/HorariosView';
 import Visitas from './Views/CalendarioView';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/AdminVisitas" component={AdminVisitas}/>
           <Route path="/Exposiciones" component={Exposiciones}/>
+          <Route path="/Guias/:id" component={Horarios}/>
           <Route path="/Guias" component={Guias}/>
           <Route path="/CalendarioVisitas" component={Visitas}/>
         </Switch>
